e47cc5c9f659cad0f927735fa5e6222044f76cae
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2025-09-28 14:23:23 +0530

    few updates codes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const { authUser } = useContext(AuthContext);
   return (
     <div
-      className="bg-[url('./src/assets/bgImage.svg')]
+      className="bg-[url('/bgImage.svg')]
      bg-contain"
     >
       <Toaster />
